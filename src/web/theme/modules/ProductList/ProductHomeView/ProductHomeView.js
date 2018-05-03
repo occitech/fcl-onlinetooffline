efd2858f9a6e42d479068f96edd342e6672c9706
emile <emile31.roch@gmail.com>
2018-05-03 11:52:38 +0200

    Create productHomeView - refs #11962

---
+++ src/web/theme/modules/ProductList/ProductHomeView/ProductHomeView.js
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "theme/ui/atoms/Button";
+import IconWithLabel from "theme/ui/molecules/IconWithLabel";
+import Link from "theme/ui/atoms/Typography/Link";
+import "./ProductHomeView.scss";
+
+const ProductHomeView = ({
+  accentTitle,
+  Title,
+  contentProductIntro,
+  product,
+  isCurrent,
+  nextProduct,
+  viewProduct
+}) => {
+  const productHomeViewClasses = `product-home-view--${
+    isCurrent ? "current" : "inactive"
+  }`;
+  return (
+    <div className={productHomeViewClasses}>
+      <div className="product-home-view__title">
+        <strong>{accentTitle}</strong>
+        {Title}
+      </div>
+      <div className="product-home-view__content">
+        {contentProductIntro} <strong>{product.name}</strong>
+      </div>
+      <div className="product-home-view__action">
+        <div className="product-home-view__action__view-product">
+          <Link to={`product/${product.sku}`} type="important">
+            View Product
+          </Link>
+        </div>
+        <div className="product-home-view__action__next-product">
+          <Button
+            onClick={() => {
+              nextProduct();
+            }}
+            type="simple"
+          >
+            <IconWithLabel icon="arrow-down" type="down">
+              Next Product
+            </IconWithLabel>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Button.propTypes = {
+  product: PropTypes.node,
+  isCurrent: PropTypes.bool,
+  nextProduct: PropTypes.func,
+  viewProduct: PropTypes.func
+};
+
+export default ProductHomeView;
