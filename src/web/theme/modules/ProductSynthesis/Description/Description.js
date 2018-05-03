1b532d8db9e652c1123b493b1be8a2a0eb4206fe
emile <emile31.roch@gmail.com>
2018-05-03 16:04:29 +0200

    Edit description layout - refs #11907

--- src/web/theme/modules/ProductSynthesis/Description/Description.js
+++ src/web/theme/modules/ProductSynthesis/Description/Description.js
@@ -2,15 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Description.scss";
 
-const Description = ({ children }) =>
-  children && (
-    <div
-      className="product-description"
-      dangerouslySetInnerHTML={{
-        __html: children
-      }}
-    />
-  );
+const Description = ({ children }) => (
+  <div className="product-view__description">
+    <span className="product-view__description__title">
+      Professional review
+    </span>
+    {children && (
+      <div
+        className="product-description"
+        dangerouslySetInnerHTML={{
+          __html: children
+        }}
+      />
+    )}
+  </div>
+);
 
 Description.propTypes = {
   children: PropTypes.node.isRequired
