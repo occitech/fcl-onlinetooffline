e02cef9a0a670fc792f5e32c4c6a53076424909a
emile <emile31.roch@gmail.com>
2018-05-03 11:48:17 +0200

    Handle the position of the icon on the iconwithlabel component - refs #11962

--- src/web/theme/ui/molecules/IconWithLabel/IconWithLabel.js
+++ src/web/theme/ui/molecules/IconWithLabel/IconWithLabel.js
@@ -3,18 +3,28 @@ import PropTypes from "prop-types";
 import Icon from "theme/ui/atoms/Icon";
 import "./IconWithLabel.scss";
 
-const IconWithLabel = ({ icon, children }) => {
-  return (
-    <span className="icon-with-label">
-      <Icon icon={icon} />
-      {children}
-    </span>
-  );
+const IconWithLabel = ({ icon, children, type = "up" }) => {
+  if (type === "up") {
+    return (
+      <span className="icon-with-label">
+        <Icon icon={icon} />
+        <span>{children}</span>
+      </span>
+    );
+  } else if (type === "down") {
+    return (
+      <span className="icon-with-label">
+        <span>{children}</span>
+        <Icon icon={icon} />
+      </span>
+    );
+  }
 };
 
 IconWithLabel.propTypes = {
   icon: PropTypes.node.isRequired,
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  type: PropTypes.oneOf(["up", "down"])
 };
 
 export default IconWithLabel;
