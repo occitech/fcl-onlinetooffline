98a76b32ccbb19329afa29e6729d438de8a24bff
emile <emile31.roch@gmail.com>
2018-05-04 13:18:49 +0200

    Remove completly the homeBanner from the theme

--- src/web/theme/pages/Home/Home.js
+++ src/web/theme/pages/Home/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import HomeQuery from "./HomeQuery.gql";
 import EnhanceHome from "./EnhanceHome";
 import ProductList from "theme/modules/ProductList";
-import HomeBanner from "theme/modules/HomeBanner";
 import LoadingArea from "theme/ui/molecules/LoadingArea";
 import Page from "theme/ui/templates/Page";
 
