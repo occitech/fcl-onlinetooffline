e423b307a459f62b73eb32d94b76d413ade49af9
emile <emile31.roch@gmail.com>
2018-05-04 11:17:31 +0200

    Refactor the tryitpipe Component - refs #11905

--- src/web/theme/pages/TryItPipe/TryItPipe.js
+++ src/web/theme/pages/TryItPipe/TryItPipe.js
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { compose } from "recompose";
 import EnhanceTryItPipe from "./EnhanceTryItPipe";
-
+import Button from "theme/ui/atoms/Button";
 const steps = ["When", "Where", "Why"];
 
-const TryItPipe = ({ currentStep }) => {
+let step = ({ currentStep, gotoStepNumber, tryItState, setTryItState }) => {
   switch (currentStep) {
     case steps[0]:
       return <div>Work In Progress</div>;
@@ -16,6 +16,39 @@ const TryItPipe = ({ currentStep }) => {
     default:
       return null;
   }
+};
+
+let TryItPipe = ({
+  currentStep,
+  gotoStepNumber,
+  tryItState,
+  setTryItState,
+  getCurrentStepIndex
+}) => {
+  return (
+    <div className="try-it">
+      <div className="try-it__header">
+        <div className="try-it__header__title">Try it in our store</div>
+        <div className="try-it__header__steps-count">{`Step ${getCurrentStepIndex() +
+          1}/${steps.length}`}</div>
+      </div>
+      {step({
+        currentStep,
+        gotoStepNumber,
+        tryItState,
+        setTryItState,
+        getCurrentStepIndex
+      })}
+      <div className="try-it__footer">
+        <Button
+          onClick={() => {
+            gotoStepNumber(getCurrentStepIndex() + 1);
+          }}
+          type="dark"
+        >{`Step ${getCurrentStepIndex() + 2} : `}</Button>
+      </div>
+    </div>
+  );
 };
 
 TryItPipe.propTypes = { currentStep: PropTypes.string.isRequired };
