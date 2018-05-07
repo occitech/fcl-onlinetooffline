424f2b7b9856de65584696623920d38d9a568e4c
emile <emile31.roch@gmail.com>
2018-05-07 10:16:46 +0200

    Create select Date following the select store architecture - refs #11903

--- src/web/theme/pages/TryIt/SelectDate/SelectDate.js
+++ src/web/theme/pages/TryIt/SelectDate/SelectDate.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Step from "../Step";
 
-const SelectDate = () => {
-  return <div>aaaa</div>;
+const SelectDate = ({
+  step = "When",
+  currentStep,
+  gotoStepNumber,
+  tryItState,
+  setTryItState,
+  getStepIndex,
+  collapsed = false
+}) => {
+  return (
+    <div className="select-date">
+      <Step
+        step={step}
+        currentStep={currentStep}
+        stepIndex={getStepIndex(step) + 1}
+        actionDescription="Select a date on the calendar"
+        collapsed={collapsed}
+      >
+        <div className="select-date__calendar">CALENDAR</div>
+      </Step>
+    </div>
+  );
 };
 
 export default SelectDate;
