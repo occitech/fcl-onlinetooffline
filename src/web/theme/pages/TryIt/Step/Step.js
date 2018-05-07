912a8eee72efe0a56e4a28bfc17fcd4d25aea464
emile <emile31.roch@gmail.com>
2018-05-07 10:13:25 +0200

    Create step component - refs #11903

---
+++ src/web/theme/pages/TryIt/Step/Step.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from "react";
+import "./Step.scss";
+
+const Step = ({
+  step,
+  currentStep,
+  stepIndex,
+  actionDescription,
+  collapsed,
+  children
+}) => {
+  const stepTitleClasses = `step__title ${
+    currentStep === step ? "" : "step__title--validated"
+  }`;
+
+  return (
+    <Fragment>
+      <div className={stepTitleClasses}>
+        <span className="step__title__pins">{stepIndex}</span>
+        <span className="step__title__content">{actionDescription}</span>
+      </div>
+      {!collapsed ? children : null}
+    </Fragment>
+  );
+};
+
+export default Step;
