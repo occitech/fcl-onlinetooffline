4a51b8c104924b4667430e7d1c6e072feb046a28
emile <emile31.roch@gmail.com>
2018-06-26 16:08:17 +0200

    Add cart modal opening on the button activation - refs #11904

--- src/web/theme/pages/OrderReference/OrderReference.js
+++ src/web/theme/pages/OrderReference/OrderReference.js
@@ -5,6 +5,7 @@ import Button from "theme/ui/atoms/Button";
 import AddToCartMutation from "theme/ui/molecules/AddToCart/AddToCartMutation.gql";
 import { Mutation } from "react-apollo";
 import EnhanceOrderReference from "./EnhanceOrderReference";
+import { CartModal } from "theme/modules/Cart";
 
 const OrderReference = ({ customerOrder, checkCustomerID }) => {
   return (
@@ -16,26 +17,30 @@ const OrderReference = ({ customerOrder, checkCustomerID }) => {
         Your order is waiting for you
       </div>
       <div className="order-reference__instruction">fill your order code</div>
-      <div className="order-reference__action">
-        <Mutation mutation={AddToCartMutation} onCompleted={() => {}}>
-          {(addToCart, { loading }) => (
-            <Button
-              onClick={e => {
-                e.preventDefault();
-                customerOrder.products && customerOrder.products.length > 0
-                  ? customerOrder.products.map(product =>
-                      addToCart({ variables: { sku: product.sku } })
-                    )
-                  : () => {};
-              }}
-              status={loading ? "loading" : ""}
-              type="dark"
-            >
-              Use your code
-            </Button>
+      <div className={actionClassName}>
+        <CartModal>
+          {openCart => (
+            <Mutation mutation={AddToCartMutation} onCompleted={openCart}>
+              {(addToCart, { loading }) => (
+                <Button
+                  onClick={e => {
+                    e.preventDefault();
+                    setnoProductFound(false);
+                    customerOrder.products && customerOrder.products.length > 0
+                      ? customerOrder.products.map(product =>
+                          addToCart({ variables: { sku: product.sku } })
+                        )
+                      : setnoProductFound(true);
+                  }}
+                  status={loading ? "loading" : ""}
+                  type="dark"
+                >
+                  Use your code
+                </Button>
+              )}
+            </Mutation>
           )}
-        </Mutation>
-
+        </CartModal>
         <input onChange={checkCustomerID} type="number" id="customerOrderId" />
       </div>
     </div>
