19f5134173e4c496a7b22515084f0b7593ff1908
emile <emile31.roch@gmail.com>
2018-06-28 16:08:11 +0200

    Fix button comportement - refs #11906

--- src/web/theme/pages/TryIt/TryIt.js
+++ src/web/theme/pages/TryIt/TryIt.js
@@ -97,7 +97,7 @@ const TryIt = props => {
         </div>
       ) : null}
       {steps[props.currentStep].renderStep(props)}
-      <Transition type="fade-in-top" delay={1} duration={0.5}>
+      <Transition type="fade-in-top" delay={0.2} duration={0.5}>
         <div className="try-it__footer">
           <Button
             onClick={() => {
