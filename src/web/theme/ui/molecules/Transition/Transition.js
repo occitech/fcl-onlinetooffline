78e58966394bc66a794828d9f1fb7fc56496dfc9
emile <emile31.roch@gmail.com>
2018-06-28 15:55:25 +0200

    Refactor transition component - refs #11906

--- src/web/theme/ui/molecules/Transition/Transition.js
+++ src/web/theme/ui/molecules/Transition/Transition.js
@@ -1,25 +1,69 @@
 import React from "react";
 import Observer from "@researchgate/react-intersection-observer";
-import { compose, withState, withHandlers } from "recompose";
+import { compose, withState, withHandlers, lifecycle } from "recompose";
 import "./Transition.scss";
+window.requestAnimationFrame =
+  window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.msRequestAnimationFrame;
 
-const Transition = ({ children, handleChange, isVisible, type, delayed }) => {
+const Transition = ({
+  children,
+  handleChange,
+  isVisible,
+  type,
+  delay,
+  duration = 1,
+  animationState
+}) => {
   const transitionClass = `transition ${
-    isVisible && type ? "transition--" + type : ""
-  } ${delayed ? "transition--delayed" : ""}`;
+    isVisible && type
+      ? "transition--" +
+        type +
+        ` ${animationState ? `transition--${type}${animationState}` : ""}`
+      : "transition--" + type + ` transition--${type}--enter`
+  }`;
 
   return (
     <Observer onChange={handleChange}>
-      <div className={transitionClass}>{children}</div>
+      <div
+        style={{
+          transitionDelay: `${delay}s`,
+          transitionDuration: `${duration}s`
+        }}
+        className={transitionClass}
+      >
+        {children}
+      </div>
     </Observer>
   );
 };
 
 export default compose(
   withState("isVisible", "setVisibility", false),
+  withState("animationState", "setAnimationState", "enter"),
   withHandlers({
     handleChange: props => event => {
       props.setVisibility(event.isIntersecting ? true : false);
+    }
+  }),
+  lifecycle({
+    componentDidUpdate(prevProps) {
+      var start = null;
+      const requestTick = timestamp => {
+        if (start === null) start = timestamp;
+        if (timestamp > start + 20) {
+          this.props.setAnimationState(null);
+        } else {
+          requestAnimationFrame(requestTick);
+        }
+      };
+
+      if (!prevProps.isVisible && this.props.isVisible) {
+        this.props.setAnimationState("--enter");
+        requestAnimationFrame(requestTick);
+      }
     }
   })
 )(Transition);
